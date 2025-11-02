b00856f4aee5a4f84597811def4966cbe3106de0
ArifurRahmanShezan <arifurrahmanshezan@gmail.com>
2025-11-02 10:25:33 +0600

    all sort now

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -4,11 +4,13 @@ import { RouterModule, Routes } from '@angular/router';
 import { BudgetComponent } from './acquisitions/budget/budget.component';
 import { GiftComponent } from './acquisitions/gift/gift.component';
 import { AccessioningComponent } from './acquisitions/accessioning/accessioning.component';
+import { RestoreComponent } from './database/restore/restore.component';
+
 
 
 
 const routes: Routes = [
-  //{ path: 'database-restore', component: RestoreComponent },
+  { path: 'database-restore', component: RestoreComponent },
   { path: 'budget-vendor', component: BudgetComponent} ,
   { path:'gift',component:GiftComponent},
   {path:'accesssioning',component:AccessioningComponent},
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,20 +4,20 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { MenuListComponent } from './components/menu-list/menu-list.component';
-// import { RestoreComponent } from './components/database/restore/restore.component';
 import { BudgetComponent } from './acquisitions/budget/budget.component';
 import { GiftComponent } from './acquisitions/gift/gift.component';
 import { AccessioningComponent } from './acquisitions/accessioning/accessioning.component';
 import { FormsModule } from '@angular/forms';
+import { RestoreComponent } from './database/restore/restore.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     MenuListComponent,
-    //RestoreComponent,
     BudgetComponent,
     GiftComponent,
-    AccessioningComponent
+    AccessioningComponent,
+    RestoreComponent
   ],
   imports: [
     BrowserModule,
